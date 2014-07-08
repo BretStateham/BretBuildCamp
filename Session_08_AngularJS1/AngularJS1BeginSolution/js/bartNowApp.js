44fea5eef505b22c432a8b49cdaeaea35dc6070f
Bret Stateham <Bret.Stateham@microsoft.com>
2014-07-08 03:07:58 +0000

    Modified demo code to show departures

--- Session_08_AngularJS1/AngularJS1BeginSolution/js/bartNowApp.js
+++ Session_08_AngularJS1/AngularJS1BeginSolution/js/bartNowApp.js
@@ -5,17 +5,46 @@ var bartNowApp = angular.module("bartNowApp", []);
 bartNowApp.controller("MainCtrl", ['$scope', function ($scope) {
 
   //Add a static model for a single bart station 
-  //This time with an array of stations though, not just one
-  $scope.stations = [
-    { name: "12th St. Oakland City Center", abbr: "12TH", zipcode: "94612" },
-    { name: "16th St. Mission", abbr: "16TH", zipcode: "94110" },
-    { name: "19th St. Oakland", abbr: "19TH", zipcode: "94612" },
-    { name: "24th St. Mission", abbr: "24TH", zipcode: "94110" },
-    { name: "Ashby", abbr: "ASHB", zipcode: "94703" },
-    { name: "Balboa Park", abbr: "BALB", zipcode: "94112" },
-    { name: "Bay Fair", abbr: "BAYF", zipcode: "94578" },
-    { name: "Castro Valley", abbr: "CAST", zipcode: "94546" },
-    { name: "Civic Center/UN Plaza", abbr: "CIVC", zipcode: "94102" },
-    { name: "Coliseum/Oakland Airport", abbr: "COLS", zipcode: "94621" }];
+  //This time with an array of stations each with it's
+  //own nested array of estimate times of departure for 
+  //multiple remote stations, as welll as individual estimates
+  //for individual trains on to those target destinations
+  //The sample data here is trimmed significantly to help
+  //visualize it.
+  $scope.stations = [{
+    name: "12th St. Oakland City Center",
+    abbr: "12TH",
+    etd: [
+      {
+        destination: "24th Street",
+        estimate: [
+          { minutes: "25", color: "YELLOW" },
+          { minutes: "40", color: "YELLOW" }]
+      },
+      {
+        destination: "Fremont",
+        estimate: [
+          { minutes: "5", color: "ORANGE" },
+          { minutes: "20", color: "ORANGE" }]
+      }]
+  },
+  {
+    name: "16th St. Mission",
+    abbr: "16TH",
+    etd: [
+      {
+        destination: "24th Street",
+        estimate: [
+          { minutes: "43", color: "YELLOW" },
+          { minutes: "58", color: "YELLOW" }]
+      },
+      {
+        destination: "Daly City",
+        estimate: [
+          { minutes: "3", color: "GREEN" },
+          { minutes: "9", color: "BLUE" }]
+      }]
+  }
+  ];
 
 }]);
