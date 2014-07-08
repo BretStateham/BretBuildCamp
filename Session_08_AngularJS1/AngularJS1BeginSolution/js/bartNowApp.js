14211d35e9c6b39ddfbbaea784b6301d5d995229
Bret Stateham <Bret.Stateham@microsoft.com>
2014-07-08 00:58:55 +0000

    Working on AngularJS1 Demo Code
    Includes the addition of the Angular.js 1.2.19 and an initial
    stations.json asset.

---
+++ Session_08_AngularJS1/AngularJS1BeginSolution/js/bartNowApp.js
@@ -0,0 +1,18 @@
+﻿//Create a new Angular Module for the bartNowApp. 
+var bartNowApp = angular.module("bartNowApp", []);
+
+//Create the MainCtrl Controller...
+bartNowApp.controller("MainCtrl", ['$scope', function ($scope) {
+
+  //Add a static model for a single bart station 
+  $scope.station = {
+    longitude: -122.271604,
+    latitude: 37.803664,
+    name: "12th St. Oakland City Center",
+    abbr: "12TH",
+    county: "alameda",
+    state: "CA",
+    zipcode: "94612"
+  };
+
+}]);
